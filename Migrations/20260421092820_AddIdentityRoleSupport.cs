a812ff84b60e38fcd1cd4aeb4a329840fcfccec2
Danny <dannymuchoki@users.noreply.github.com>
2026-04-21 08:02:03 -0400

    Added migration downloader

--- Migrations/20260421092820_AddIdentityRoleSupport.cs
+++ Migrations/20260421092820_AddIdentityRoleSupport.cs
@@ -1,22 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace RecordsMaster.Migrations
 {
-    /// <inheritdoc />
     public partial class AddIdentityRoleSupport : Migration
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-
-        }
-
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-
-        }
+        protected override void Up(MigrationBuilder migrationBuilder) { }
+        protected override void Down(MigrationBuilder migrationBuilder) { }
     }
 }
